dbcbc208fcf1e9184acfc73d9268acf4ae01b480
andrigoBS <andrigo2000@gmail.com>
2021-08-30 03:55:11 -0300

    Docker complete version 1.0

--- src/controllers/institution/routes.js
+++ src/controllers/institution/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/users', require('./UserController'));
+router.use('/views', require('./UserController'));
 
 module.exports = router;

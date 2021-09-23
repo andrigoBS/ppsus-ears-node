d4b7af65a3d37f42229035fdd123dd4551a5361a
Hilson <hilson.alex234@gmail.com>
2021-09-23 00:21:07 -0300

    Creating and testing migrations

---
+++ src/migration/1632365191327-UserCreate.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class UserCreate1632365191327 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "user",
+            columns: [
+                {name: "id", type: "int", isPrimary: true, isGenerated: true},
+                {name: "name", type: "varchar", length: "255"}
+            ]
+        }), true);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("user");
+    }
+
+}

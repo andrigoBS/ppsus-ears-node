8b5d30a058fdfdd3ba8ec6bb45e90ba344d7bc21
Hilson <hilson.alex234@gmail.com>
2022-03-28 21:54:49 -0300

    Criado template de usuário

---
+++ src/entity/decorators/templates/UserTemplate.ts
@@ -0,0 +1,36 @@
+import {BaseEntity, BeforeInsert, Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn} from "typeorm";
+
+/**
+ * Entidade abstrata representando um usuário.
+ *
+ * Esta entidade não representa uma tabela no banco, mas serve de
+ * template para várias tabelas.
+ */
+export abstract class UserTemplate extends BaseEntity {
+
+    @PrimaryGeneratedColumn({name: "id_usuario",
+        comment: "Chave primária de um usuário. é única apenas dentro de uma tabela."
+    })
+    id: number;
+
+    @Column()
+    password: string;
+
+    // Controle
+
+    @CreateDateColumn({name: "data_cadastro", type: "datetime",
+        comment: "Data de cadastro do usuário"
+    })
+    registrationDate: Date;
+
+    @DeleteDateColumn({name: "data_desativado", type: "datetime", nullable: true,
+        comment: "Coluna usada para o Soft Delete, caso tenha um valor, o usuário foi inativado nessa data"
+    })
+    disableDate: Date;
+
+    @BeforeInsert()
+    hashPassword() {
+        console.log(this)
+    }
+
+}

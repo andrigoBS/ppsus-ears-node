b9550cc4577ae1b06c95c3bbd4201ec588c93231
Vincius Hostins Pereira <vinihp13@gmail.com>
2022-07-21 20:50:22 -0300

    Alterações feitas no referral-service e rota criada

--- src/entity/referral_service/ReferralService.ts
+++ src/entity/referral_service/ReferralService.ts
@@ -8,9 +8,15 @@ import {
     PrimaryGeneratedColumn,
 } from 'typeorm';
 
-import { ReferralServiceEmail as Email } from './ReferralServiceEmail';
-import { ReferralServicePhone as Phone } from './ReferralServicePhone';
-import { AddressComponent as Address } from '../decorators/components/Address';
+import {ReferralServiceEmail as Email} from './ReferralServiceEmail';
+import {ReferralServicePhone as Phone} from './ReferralServicePhone';
+import {AddressComponent as Address} from '../decorators/components/Address';
+
+export enum Service {
+    SUS = 'Serviço do Sistema Único de Saúde(SUS)',
+    PRIVATE = 'Serviço Privado',
+    MIXED = 'Serviço Misto',
+}
 
 /**
  * Serviço de referência para o qual o bebê é encaminhado
@@ -19,59 +25,55 @@ import { AddressComponent as Address } from '../decorators/components/Address';
 @Entity('servico_referencia')
 export class ReferralService extends BaseEntity {
 
-    @PrimaryGeneratedColumn({ name: 'id_servico',
+    @PrimaryGeneratedColumn({name: 'id_servico',
         comment: 'Chave primária do servico de referencia',
     })
-        id: number;
+    id: number;
 
-    @Column({ name: 'nome_fantasia', type: 'varchar', length: 255,
-        comment: 'Nome fantasia do endereço de referencia',
+    @Column({
+        name: 'nome_servico', type: 'varchar', length: 255,
+        comment: 'Nome do Serviço',
     })
-        name: string;
+    name: string;
 
-    @Column({ name: 'razao_social', type: 'varchar', length: 255, nullable: true,
-        comment: 'Razão social do endereço de referencia',
-    })
-        socialName: string;
-
-    @Column({ name: 'cnpj', type: 'varchar', length: 13, nullable: true,
+    @Column({name: 'cnpj', type: 'varchar', length: 13, nullable: true,
         comment: 'CNPJ do servico de referencia',
     })
-        cnpj: string;
+    cnpj: string;
 
-    @Column({ name: 'cnes', type: 'varchar', length: 7, nullable: true })
-        cnes: string;
+    @Column({name: 'cnes', type: 'varchar', length: 7, nullable: true})
+    cnes: string;
 
-    @Column({ name: 'is_sus', type: 'boolean', default: false,
-        comment: 'Se o serviço de referencia é referente ao SUS  Privado',
+    @Column({ name: 'tipo_servico', type: 'enum', update: false, enum: Service,
+        comment: 'Tipo de Serviço',
     })
-        isSus: boolean;
+    typeService: Service;
 
-    @Column(() => Address, { prefix: false })
-        address: Address;
+    @Column(() => Address, {prefix: false})
+    address: Address;
 
     // Controle
 
-    @CreateDateColumn({ name: 'data_cadastro', type: 'datetime',
+    @CreateDateColumn({name: 'data_cadastro', type: 'datetime',
         comment: 'Data de cadastro do serviço de referencia',
     })
-        registrationDate: Date;
+    registrationDate: Date;
 
-    @DeleteDateColumn({ name: 'data_desativado', type: 'datetime', nullable: true,
+    @DeleteDateColumn({name: 'data_desativado', type: 'datetime', nullable: true,
         comment: 'Coluna usada para o Soft Delete, caso tenha um valor, o serviço de referencia foi inativado nessa data',
     })
-        disableDate: Date;
+    disableDate: Date;
 
     // Relacionamentos
 
     @OneToMany(() => Email, (email) => email.service, {
         cascade: ['soft-remove', 'recover', 'remove'],
     })
-        emails: Email[];
+    emails: Email[];
 
     @OneToMany(() => Phone, (phone) => phone.service, {
         cascade: ['soft-remove', 'recover', 'remove'],
     })
-        phones: Phone[];
+    phones: Phone[];
 
 }

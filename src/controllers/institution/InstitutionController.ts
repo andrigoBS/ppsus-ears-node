beccf10d860a1629b527f775d2e965c88e467acf
prrThr <arthur.op9@gmail.com>
2022-07-26 14:32:37 -0300

    Rota GET para tipos de instituição

--- src/controllers/institution/InstitutionController.ts
+++ src/controllers/institution/InstitutionController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { HttpStatus } from '../../helpers/HttpStatus';
 import AbstractController from '../AbstractController';
-import { Institution } from '../../entity/institution/Institution';
+import { Institution, institutiontype } from '../../entity/institution/Institution';
 
 export default class InstitutionController extends AbstractController
 {
@@ -9,42 +9,18 @@ export default class InstitutionController extends AbstractController
     constructor()
     {
         super();
-        const { createService } = this;
+        const { createService, institutionTypes } = this;
         const { verifyJWTMiddleware } = this.getJwt();
         const router = this.getRouter();
         router.post('/', createService);
+        router.get('/types', institutionTypes);
     }
 
     private createService = async (req: Request, res: Response) =>
     {
         /*
-           #swagger.tags = ['ReferralService']
-           #swagger.description = 'Endpoint para recuperar todos os serviços de referencia'
-           #swagger.parameters['institution'] = {
-            in: 'body',
-            required: 'true',
-            description: 'Instituição',
-            type: 'object',
-            schema: {
-                "institutionName": "nome instituicao",
-                "password": "senha",
-                "passwordConfirmation": "confirmando senha",
-                "cnes": "numero cnes",
-                "cnpj": "cnpj",
-                "institutionType": 2,
-                "email": "example@gmail.com",
-                "alternative_email": "example2@gmail.com",
-                "institucional_phone": "123435",
-                "cep": "33232",
-                "public_area": "logradouro",
-                "state": "sc",
-                "city":  "floripa",
-                "number": "3242",
-                "complement": "complemento",
-                "responsible_name": "nome responsavel"
-            }
-            
-           }
+           #swagger.tags = ['Institution']
+           #swagger.description = '(???)Endpoint para recuperar todos os serviços de referencia (???)'
            #swagger.security = [{
                 "ApiKeyAuth": []
             }]
@@ -54,4 +30,18 @@ export default class InstitutionController extends AbstractController
         institution = await Institution.save(institution);
         return res.status(HttpStatus.OK).json(institution);
     }
+
+    private institutionTypes = async (req: Request, res: Response) =>{
+        /*
+            #swagger.tags = ['Institution']
+            #swagger.description = 'Tipos de instituição'
+            #swagger.security = [{
+                "ApiKeyAuth": []
+            }
+        */
+
+        return res.status(HttpStatus.OK).send(institutiontype);
+    }
 }
+
+        

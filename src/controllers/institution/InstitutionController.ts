a99ca21767a0b52fc1aa75c1329e0fe6ff80adcd
KarolineGuckert <karolinesouzaguckert@gmail.com>
2022-07-25 21:35:48 -0300

    Adjustment labels of Institution.jsx and Institution.ts

--- src/controllers/institution/InstitutionController.ts
+++ src/controllers/institution/InstitutionController.ts
@@ -28,20 +28,20 @@ export default class InstitutionController extends AbstractController
             schema: {
                 "institutionName": "nome instituicao",
                 "password": "senha",
-                "passwordConfirmation": "confirmando senha",
                 "cnes": "numero cnes",
                 "cnpj": "cnpj",
                 "institutionType": 2,
                 "email": "example@gmail.com",
-                "alternative_email": "example2@gmail.com",
-                "institucional_phone": "123435",
+                "alternativeEmail": "example2@gmail.com",
+                "institutionPhone": "123435",
                 "cep": "33232",
-                "public_area": "logradouro",
+                "publicArea": "logradouro",
                 "state": "sc",
                 "city":  "floripa",
                 "number": "3242",
                 "complement": "complemento",
-                "responsible_name": "nome responsavel"
+                "responsibleName": "nome responsavel",
+                "responsibleRole": "cargo do responsavel"
             }
             
            }
@@ -49,6 +49,8 @@ export default class InstitutionController extends AbstractController
                 "ApiKeyAuth": []
             }]
         */
+
+
 
         let institution = req.body as Institution
         institution = await Institution.save(institution);

a87d92a51c2e76f4ebd1931e94938fad2ec64fff
KarolineGuckert <karolinesouzaguckert@gmail.com>
2022-07-28 12:33:25 -0300

    Adjusting fields names

--- src/controllers/institution/InstitutionController.ts
+++ src/controllers/institution/InstitutionController.ts
@@ -19,10 +19,38 @@ export default class InstitutionController extends AbstractController {
         /*
            #swagger.tags = ['Institution']
            #swagger.description = 'Endpoint para recuperar todos os serviços de referencia'
+           #swagger.parameters['institution'] = {
+            in: 'body',
+            required: 'true',
+            description: 'Instituição',
+            type: 'object',
+            schema: {
+                "institutionName": "nome instituicao",
+                "password": "senha",
+                "cnes": "numero cnes",
+                "cnpj": "cnpj",
+                "institutionType": 2,
+                "email": "example@gmail.com",
+                "alternativeEmail": "example2@gmail.com",
+                "institutionPhone": "123435",
+                "institutionalCellphone": "123435",
+                "cep": "33232",
+                "publicArea": "logradouro",
+                "state": "sc",
+                "city":  "floripa",
+                "number": "3242",
+                "complement": "complemento",
+                "responsibleName": "nome responsavel",
+                "responsibleRole": "cargo do responsavel"
+            }
+
+           }
            #swagger.security = [{
                 "ApiKeyAuth": []
             }]
         */
+
+        console.log("OOOOOOOOOOOOOOO " + req.body.institutionType)
 
         let institution = req.body as Institution;
         institution = await Institution.save(institution);
